46d1a20dcb86c6296724cf6803d2b2e94c7cf565
Kyle McClellan <kylemcclellan3@gmail.com>
2023-07-27 20:04:38 -0500

    Logger provider shell

---
+++ Elasticstretch.Logging.Filebeat/FilebeatLoggerProvider.cs
@@ -0,0 +1,36 @@
+﻿namespace Elasticstretch.Logging.Filebeat;
+
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// A provider of loggers for use with Filebeat.
+/// </summary>
+/// <remarks>
+/// Log entries are documents satisfying the Elastic Common Schema, written to the filesystem as UTF-8 newline-delimited JSON.
+/// </remarks>
+[ProviderAlias("Filebeat")]
+public class FilebeatLoggerProvider : ILoggerProvider
+{
+    /// <inheritdoc/>
+    public ILogger CreateLogger(string categoryName)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Disposes and/or finalizes the instance.
+    /// </summary>
+    /// <param name="disposing">
+    /// <see langword="true"/> to dispose and finalize, <see langword="false"/> to finalize only.
+    /// </param>
+    protected virtual void Dispose(bool disposing)
+    {
+    }
+}
