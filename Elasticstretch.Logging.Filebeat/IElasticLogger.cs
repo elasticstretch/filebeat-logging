8c09a9bfd6a5136f4e531f856f72753b4f2ddcdb
Kyle McClellan <kylemcclellan3@gmail.com>
2023-07-29 13:18:58 -0500

    Add scope category to elastic logger

--- Elasticstretch.Logging.Filebeat/IElasticLogger.cs
+++ Elasticstretch.Logging.Filebeat/IElasticLogger.cs
@@ -11,17 +11,18 @@ public interface IElasticLogger
     /// <summary>
     /// Determines if any fields exist for a log category.
     /// </summary>
-    /// <param name="categoryName">The log category name.</param>
+    /// <param name="category">The log category name.</param>
     /// <returns><see langword="true"/> if one or more fields exist, otherwise <see langword="false"/>.</returns>
-    bool IsEnabled(string categoryName);
+    bool IsEnabled(string category);
 
     /// <summary>
     /// Formats a log scope into document fields.
     /// </summary>
     /// <typeparam name="TState">The scope state type.</typeparam>
     /// <param name="fieldFactory">A delegate to retrieve a JSON writer for an Elastic field.</param>
+    /// <param name="category">The log category name.</param>
     /// <param name="state">The log scope state.</param>
-    void WriteScope<TState>(Func<string, Utf8JsonWriter> fieldFactory, TState state)
+    void WriteScope<TState>(Func<string, Utf8JsonWriter> fieldFactory, string category, TState state)
         where TState : notnull;
 
     /// <summary>
