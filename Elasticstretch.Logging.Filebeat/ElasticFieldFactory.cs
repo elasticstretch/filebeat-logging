54f458a3e8943a1cbb8a5c79d96f125cefc397db
Kyle McClellan <kylemcclellan3@gmail.com>
2023-07-30 15:54:55 -0500

    Elastic logger -> log writer
    * Remove category filtering
    * Add static fields

---
+++ Elasticstretch.Logging.Filebeat/ElasticFieldFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Elasticstretch.Logging.Filebeat;
+
+using System.Text.Json;
+
+/// <summary>
+/// Creates or retrieves a JSON writer for an Elastic field.
+/// </summary>
+/// <param name="fieldName">The Elastic field name.</param>
+/// <returns>The JSON field writer.</returns>
+public delegate Utf8JsonWriter ElasticFieldFactory(string fieldName);
---
+++ Elasticstretch.Logging.Filebeat/IElasticLogWriter.cs
@@ -0,0 +1,33 @@
+﻿namespace Elasticstretch.Logging.Filebeat;
+
+using Microsoft.Extensions.Logging.Abstractions;
+
+/// <summary>
+/// Writes log information as Elastic document fields.
+/// </summary>
+public interface IElasticLogWriter
+{
+    /// <summary>
+    /// Writes static document fields for all log entries.
+    /// </summary>
+    /// <param name="factory">The Elastic field factory.</param>
+    void WriteStatic(ElasticFieldFactory factory);
+
+    /// <summary>
+    /// Writes document fields for a log scope.
+    /// </summary>
+    /// <typeparam name="TState">The scope state type.</typeparam>
+    /// <param name="factory">The Elastic field factory.</param>
+    /// <param name="category">The log category name.</param>
+    /// <param name="state">The log scope state.</param>
+    void WriteScope<TState>(ElasticFieldFactory factory, string category, TState state)
+        where TState : notnull;
+
+    /// <summary>
+    /// Writes document fields for a log entry.
+    /// </summary>
+    /// <typeparam name="TState">The entry state type.</typeparam>
+    /// <param name="factory">The Elastic field factory.</param>
+    /// <param name="entry">The log entry.</param>
+    void WriteEntry<TState>(ElasticFieldFactory factory, in LogEntry<TState> entry);
+}
